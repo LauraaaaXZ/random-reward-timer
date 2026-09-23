b42de1c56e32400439d390517510ee8c3fa6b667
LauraaaaXZ <Zhaox1998@outlook.com>
2026-09-23 19:16:51 +0800

    fix: avoid JSX in ts entrypoint

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 import {ThemeProvider} from'./src/theme/ThemeContext';
-function Root(){return <ThemeProvider><App/></ThemeProvider>}
+function Root(){return React.createElement(ThemeProvider,null,React.createElement(App))}
 registerRootComponent(Root);
